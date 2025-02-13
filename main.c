9d2d1b2023d0a592e132aae123edf46a82975efd
Sebastian <sebastian.wintgen@informatik.hs-fulda.de>
2025-02-13 21:46:07 +0100

    Datenuebertragung

--- main.c
+++ main.c
@@ -10,10 +10,30 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <time.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
 
 #include "libgpiod.h"
 
-int arr[10] = {0,1,1,1,0,0,0,1,1,0};
+int senden = 0;
+
+int arr[10] = {1,1,0,0,0,1,0,1,0,1};
+int message[32];
+
+void stringToBinaer(uint8_t *array, const char *binaryString) {
+    size_t length = strlen(binaryString);
+    for (size_t i = 0; i < length; i++) {
+        if (binaryString[i] == '0') {
+            array[i] = 0;
+        } else if (binaryString[i] == '1') {
+            array[i] = 1;
+        } else {
+            printf("Ungültiges Zeichen: %c\n", binaryString[i]);
+            return;
+        }
+    }
+}
 
 // Structure to hold thread data
 struct thread_data
@@ -39,7 +59,7 @@ void* thread_function(void* arg) {
     int anzahl = 0;
     for(;;)
     {
-        if(anzahl % 2 == 0)
+        /*if(anzahl % 2 == 0)
         {
             // Set GPIO line to high
             schlafe(0,300000);
@@ -61,6 +81,14 @@ void* thread_function(void* arg) {
             // Set GPIO line to low
             set_gpio(thread_data.line, 0);
             schlafe(0,200000);
+        }*/
+
+        if(senden == 0)
+        {
+            set_gpio(thread_data.line, 0);
+        } else
+        {
+            set_gpio(thread_data.line, 1);
         }
 
         anzahl++;
@@ -75,9 +103,10 @@ void* thread_function2(void* arg) {
 
     int anzahl = 0;
 
+    senden = 1;
     for(;;)
     {
-        if(arr[0] == 0)
+        if(message[anzahl] == 0)
         {
             // Set GPIO line to high
             schlafe(0,300000);
@@ -100,14 +129,44 @@ void* thread_function2(void* arg) {
             set_gpio(thread_data.line, 0);
             schlafe(0,200000);
         }
-        if (anzahl < 10)
+        if (anzahl < 32)
         {
             anzahl++;
+        } else
+        {
+            senden = 0;
         }
     }
 }
 
 int main() {
+
+    // Message inistialisieren
+    // ID 0X99 1100 011 8 Bit
+    message[0] = 0;
+    message[1] = 1;
+    message[2] = 1;
+    message[3] = 0;
+    message[4] = 0;
+    message[5] = 0;
+    message[6] = 1;
+    message[7] = 1;
+
+    // Wert-ID 1 4 Bit
+    message[8] = 0;
+    message[9] = 0;
+    message[10] = 0;
+    message[11] = 1;
+
+    // Wert 1 8 Bit
+
+    // Wert-ID 1 4 Bit
+    message[20] = 0;
+    message[21] = 0;
+    message[22] = 1;
+    message[23] = 0;
+    // Wert 8 Bit
+
     pthread_t thread1, thread2;
     struct thread_data thread1_args, thread2_args;
 
